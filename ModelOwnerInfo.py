3d46b3b4ffea82dfa59f4626c36bd998b3a293da
A1sky <85579771+A1sky@users.noreply.github.com>
2021-06-08 23:45:43 -0400

    Create ModelOwnerInfo.py

---
+++ ModelOwnerInfo.py
@@ -0,0 +1,8 @@
+def GetModelOwner(Cookie, ID):
+    SellerRequest = requests.get(f"https://api.roblox.com/Marketplace/ProductInfo?assetId={ID}", cookies = {
+        ".ROBLOSECURITY": Cookie
+    })
+
+    Data = SellerRequest.json()
+
+    return Data
